8d9619ba4e5c8a6d4fd7e34844d523418ff757d4
Efrat Abramovitz <efrat.abramovitz@gmail.com>
2015-09-09 12:49:40 +0300

    prettier code

--- magic_square.rb
+++ magic_square.rb
@@ -1,11 +1,11 @@
 #!/usr/bin/env ruby
-require 'active_support/all'
-
 
 $arr=nil
 $size
 $x=0
 $y=0
+
+
 def pretty_print
 	$arr.each do |x|
 		print x
@@ -25,33 +25,26 @@ end
 
 def fill_square
 	while next_step?
-
-
-
 	end
 end
 
 def next_step?
-	
 	a = ($x+1) % $size 
 	b = ($y-1) % $size 
 
 	if $arr[b][a] == 0
 		$arr[b][a] = $arr[$y][$x]+1
-		$x = a
-		$y = b
-
 	else
 		a = ($x)
 		b = ($y+1)% $size 
 		if $arr[b][a] == 0
 			$arr[b][a] = $arr[$y][$x]+1
-			$x = a
-			$y = b
-
+			
 		else 
 			return false
 		end
+	$x = a
+	$y = b
 	return true
 	end
 
@@ -62,22 +55,18 @@ def main
 	puts "Choose an odd number"
 	num = gets
 	until Integer(num) != nil && Integer(num) % 2 ==1 
-		puts "Please pick an odd number"
+		puts "Try again to pick an odd number"
 		num = gets
 	end
 	
 	$size = Integer(num)
 	initialize_arr
-	pretty_print
+
 	$x = ($size/2)
 	$arr[$y][$x] = 1
 
-
 	fill_square
 	pretty_print
-
-
-
 
 end
 
